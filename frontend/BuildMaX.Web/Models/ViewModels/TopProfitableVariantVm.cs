663f185d85112d994f902d17ff60eacfe602c383
RockPiryt <rockpiryt@gmail.com>
2026-01-09 13:33:23 +0100

    Add TopProfitableVariantVm

---
+++ frontend/BuildMaX.Web/Models/ViewModels/TopProfitableVariantVm.cs
@@ -0,0 +1,8 @@
+namespace BuildMaX.Web.Models.ViewModels
+{
+    public class TopProfitableVariantVm
+    {
+        public string Variant { get; set; } = string.Empty;
+        public int Count { get; set; }
+    }
+}
